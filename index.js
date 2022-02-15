7513e23d412cc19369cb607723ae8700f966fcb5
Manlangit <manlangitv@gmail.com>
2022-02-14 22:48:12 -0500

    editing

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
+const orderRoute = require('./routes/order');
 
 mongoose
    .connect(process.env.MONGO_URL)
@@ -20,6 +22,10 @@ app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 //Product Routes
 app.use('/api/products', productRoute);
+//Cart Routes
+app.use('/api/carts', cartRoute);
+//Order Route
+app.use('/api/orders', orderRoute);
 
 app.listen(process.env.PORT || 5000, () => {
    console.log('Server is running');
