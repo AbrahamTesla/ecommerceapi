be9f742b965e6e0e1b339f09ba8b8a4a82313a51
Manlangit <manlangitv@gmail.com>
2022-02-15 22:36:24 -0500

    stripe route started

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config();
+const cors = require('cors');
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
 const cartRoute = require('./routes/cart');
 const orderRoute = require('./routes/order');
+const stripeRoute = require('./routes/stripe');
 
 mongoose
    .connect(process.env.MONGO_URL)
    .then(() => console.log('DB succesfully connected'))
    .catch(error => console.log(error));
 
+//Cors
+app.use(cors());
 //To send or post json to our database
 app.use(express.json());
 
@@ -26,6 +30,8 @@ app.use('/api/products', productRoute);
 app.use('/api/carts', cartRoute);
 //Order Route
 app.use('/api/orders', orderRoute);
+//Stripe Route
+app.use('/api/checkout', stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
    console.log('Server is running');
